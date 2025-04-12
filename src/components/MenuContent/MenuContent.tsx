ef71bb52cb07041df3fe69e476033ff98ac0dea3
csurbeck <csurbeck@uw.edu>
2025-04-12 11:11:04 -0700

    fixed link in menu nav

--- src/components/MenuContent/MenuContent.tsx
+++ src/components/MenuContent/MenuContent.tsx
@@ -6,7 +6,7 @@ function MenuContent() {
     <div className={styles.menuContainer}>
       <Link to="/HowItWorksPage" className={styles.menuMain}>How It Works</Link>
       <Link to="/StoryPage" className={styles.menuMain}>Our Story</Link>
-      <Link to="/ContentPage" className={styles.menuMain}>Contact Us</Link>
+      <Link to="/ContactPage" className={styles.menuMain}>Contact Us</Link>
       <Link to="/LoginPage" className={styles.menuMain}>Login</Link>
       <Link to="/RegisterPage" className={styles.menuMain}>Register</Link>
     </div>
