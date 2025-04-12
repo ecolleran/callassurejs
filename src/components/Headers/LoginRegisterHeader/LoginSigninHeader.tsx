8443a19e8c109c530b7dd7f9d6eadc2342fa5139
csurbeck <csurbeck@uw.edu>
2025-04-12 15:15:51 -0700

    render updates

--- src/components/Headers/LoginRegisterHeader/LoginSigninHeader.tsx
+++ src/components/Headers/LoginRegisterHeader/LoginSigninHeader.tsx
@@ -6,7 +6,7 @@ function LoginSigninHeader() {
   return (
     <header className={styles.header}>
         <div className={styles.logoContainer}>
-        <Link to="/">
+        <Link to="/" className={styles.logoLink}>
           <img src="/logo.png" alt="CallAssure Logo" className={styles.logo} />
         </Link>
       </div>
